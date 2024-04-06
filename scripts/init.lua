8c22fc6d075a97bf25b1cee93a50310f9ae54666
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-04-06 10:49:36 -0700

    Factor out common code

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,9 +1,17 @@
+-- ITEMS
+print("Loading Items")
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/settings.json")
 --Tracker:AddItems("items/starts.json")
+print("")
 
+-- MAPS
+print("Loading Maps")
 Tracker:AddMaps("maps/maps.json")
+print("")
 
+-- LOCATIONS
+print("Loading Locations")
 Tracker:AddLocations("locations/dungeon.json")
 Tracker:AddLocations("locations/lowerCastle.json")
 Tracker:AddLocations("locations/library.json")
@@ -13,6 +21,20 @@ Tracker:AddLocations("locations/caves.json")
 Tracker:AddLocations("locations/tower.json")
 Tracker:AddLocations("locations/theatre.json")
 --Tracker:AddLocations("locations/starts.json")
+print("")
 
+-- GRIDS
+print("Loading Grids")
+Tracker:AddLayouts("layouts/grids/powerups.json")
+Tracker:AddLayouts("layouts/grids/aspects.json")
+Tracker:AddLayouts("layouts/grids/consumables.json")
+Tracker:AddLayouts("layouts/settings/set_options.json")
+Tracker:AddLayouts("layouts/settings/set_tricks.json")
+Tracker:AddLayouts("layouts/maps/maps.json")
+print("")
+
+-- LAYOUTS
+print("Loading Layouts")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
+print("")
